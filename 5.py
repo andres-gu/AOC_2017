546999bf3745012cb54a4131421b8870390b3a04
Andres G <andres_gu@icloud.com>
2017-12-05 22:54:30 -0700

    Day 5 - Final

--- 5.py
+++ 5.py
@@ -18,7 +18,8 @@ def listMaker(stepIn):
     return steps
 
 steps = listMaker(steps_input)
-print(steps)
+test_steps = [0,3,0,1,-3]
+#print(steps)
 
 # Func for Part 1
 
@@ -28,14 +29,44 @@ def stepMaker(nlst):
     steps = 0
 
     while i < len(nlst): # while index < len(list)
+        print(">> NEW", "| list: ", nlst)
         print("i: ",i)
         i += jump
-        print("nlist 1: ", nlst[i])
+        #print("nlist 1: ", nlst[i])
         jump = nlst[i]
-        print("jump: ", jump)
+        #print("jump: ", jump)
         nlst[i] += 1
-        print("nlist 2: ",nlst[i])
+        #print("nlist 2: ",nlst[i])
         steps += 1
         print ("Steps: ", steps)
 
-stepMaker(steps) #function 1 call
+#stepMaker(steps) #function 1 call
+
+# Func for Part 2
+### Takes a long time to get to answer, be patient
+
+def jumpMaker(nlst):
+    jump = 0
+    i = 0 #index
+    steps = 0
+
+    while True: # while index < len(list)
+        print(">> NEW","| list: ", nlst)
+        print("i: ", i)
+        i += jump
+        #print("nlist 1: ", nlst[i])
+        jump = nlst[i]
+        #print("jump: ", jump)
+
+        if jump >= 3:
+            nlst[i] -= 1
+            #print("larger", "|| nlist 2: ", nlst[i])
+        else:
+            nlst[i] += 1
+            #print("smaller", "|| nlist 2: ", nlst[i])
+
+        steps += 1
+        print("Steps: ", steps)
+
+
+jumpMaker(steps) #function 1 call
